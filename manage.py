afc556df0de959f10dd17ae1dde6c8ab562617b9
tomoya <otninast38@gmail.com>
2019-01-11 10:02:26 +0900

    cors add s3 bucket

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest_api_project.settings.prod")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rest_api_project.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
