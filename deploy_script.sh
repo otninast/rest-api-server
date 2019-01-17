ed3b8db4493f258d9aae18a961d132b6bf943ebf
tomoya <otninast38@gmail.com>
2019-01-17 16:27:55 +0900

    add deploy script

---
+++ deploy_script.sh
@@ -0,0 +1,30 @@
+BUILD_DIR=.
+REGION=ap-northeast-1
+APP_NAME=rest-api-server
+DEPLOYMENT_GROUP=rest-api-server-group
+DEPLOYMENT_CONFIG=CodeDeployDefault.AllAtOnce
+S3_BUCKET=rest-api-server
+TAG=`date '+%Y%m%d%H%M'`
+S3_KEY=dev/${APP_NAME}_$TAG.zip
+
+
+echo "#### create CodeDeploy revision: ${S3_KEY}"
+
+aws deploy push --region ${REGION} --application-name ${APP_NAME} \
+       --s3-location s3://${S3_BUCKET}/${S3_KEY} --source ${BUILD_DIR}
+
+
+
+echo "#### deployment revision: ${S3_KEY}"
+
+ETAG=`aws deploy list-application-revisions --region ${REGION} \
+    --application-name ${APP_NAME} --s-3-bucket ${S3_BUCKET} --s-3-key-prefix ${S3_KEY} \
+    --query 'revisions[0].s3Location.eTag' --output text`
+
+
+
+aws deploy create-deployment --region ${REGION} --application-name ${APP_NAME} \
+    --s3-location bucket=${S3_BUCKET},key=${S3_KEY},bundleType=zip,eTag=${ETAG} \
+    --deployment-group-name ${DEPLOYMENT_GROUP} --deployment-config-name ${DEPLOYMENT_CONFIG}
+
+echo ""
