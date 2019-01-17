056f3186c511c19bb5b0047b69732d04f4aca1df
tomoya <otninast38@gmail.com>
2019-01-17 10:59:13 +0900

    rds

--- rest_api_project/settings/prod.py
+++ rest_api_project/settings/prod.py
@@ -1,5 +1,5 @@
-
 from .common import *
+import os
 
 DEBUG = True
 
@@ -13,6 +13,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rest-api-mysql',
+        'USER': os.environ.get('RDS_USER'),
+        'PASSWORD': os.environ.get('RDS_PASSWORD'),
+        'HOST': os.environ.get('RDS_HOST'),
+        'PORT': os.environ.get('RDS_PORT'),
     }
 }
 
