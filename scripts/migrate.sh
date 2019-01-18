5e0e018475617561eba120fc35eda8b9744add84
tomoya <otninast38@gmail.com>
2019-01-18 19:20:03 +0900

    improve appspec.yml

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+echo "---------- start migrate -------------"
+
+
 cd /home/ec2-user/rest-api-server
+
+pwd
+python -V
+
 python3 manage.py makemigrations
 python3 manage.py migrate
-gunicorn --daemon -c gunicorn.conf rest_api_project.wsgi:application --log-file -
+/home/ec2-user/.local/bin/gunicorn --daemon -c gunicorn.conf rest_api_project.wsgi:application --log-file -
+
+echo "---------- finish migrate -------------"
 # source /home/ec2-user/www/project-venv/bin/activate
 # DJANGO_SETTINGS_MODULE=project.settings.staging \
 # SECRET_KEY=your-secret-here \
